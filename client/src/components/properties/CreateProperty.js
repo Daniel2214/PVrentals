fe22fc6ade2892436290f991d7f511f1872bc14c
Daniel Jimenez <djimenez@earnest.com>
2021-06-24 21:44:37 -0500

    create property view using Final Form

--- client/src/components/properties/CreateProperty.js
+++ client/src/components/properties/CreateProperty.js
@@ -1,64 +1,124 @@
-import React, {Fragment} from 'react'
-import {Link} from 'react-router-dom';
-import ImageUploader from 'react-images-upload';
+import React from "react";
+import { Form, Field } from "react-final-form";
+import Styles from "./Styles";
+import ImageUploader from "react-images-upload";
 
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const CreateProperty = () => {
+const onSubmit = async (values) => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+};
 
-  const onDrop = (picture) =>  {
-    console.log(picture)
-}
+const onDrop = (picture) => {
+  console.log(picture);
+};
 
-  return (
-    <Fragment>
-      <h1 className="large text-primary">
-        Add new property
-      </h1>
-      <p className="lead">
-        Let's add some property's information
-      </p>
-      <form className="form">
-        <div className="form-group">
-          <select name="status">
-            <option value="0">* Select Property Status</option>
-            <option value="Rent">For Rent</option>
-            <option value="Sell">For Sell</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Title" name="title" />
-        </div>
-        <div className="form-group">
-          <input type="number" placeholder="Price" name="price" />
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Currency" name="currency" />
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Period of time" name="period" />
-        </div>
-        <div className="form-group">
-          <input type="text" placeholder="Location" name="location" />
-        </div>
-        <div className="form-group">
-          <textarea type="text" placeholder="Description" name="description"></textarea>
-          <small className="form-text">Describe the property: number of rooms, amenities, parking slots, etc.</small>
-        </div>
+const required = (value) => (value ? undefined : "Required");
 
-        <ImageUploader
-          withIcon={false}
-          buttonText='Choose images'
-          onChange={onDrop}
-          imgExtension={['.jpg', '.gif', '.png', '.gif']}
-          maxFileSize={5242880}
-          withPreview={true}
-        />
-        
-        <input type="submit" className="btn btn-primary my-1" />
-        <Link className="btn btn-light my-1" to="/properties">Go Back</Link>
-      </form>
-    </Fragment>
-  )
-}
+const periodQuestion = (
+  <Field name="rentPaymentPeriod" validate={required}>
+    {({ input, meta }) => (
+      <div>
+        <label>Payment Period</label>
+        <select {...input} name="rentPaymentPeriod">
+          <option />
+          <option value="month">Month</option>
+          <option value="year">Year</option>
+        </select>
+        {meta.error && meta.touched && <span>{meta.error}</span>}
+      </div>
+    )}
+  </Field>
+);
 
-export default CreateProperty
+const CreateProperty = () => (
+  <Styles>
+    <h1>Create property</h1>
+    <p>Lets add some property's information</p>
+
+    <Form
+      onSubmit={onSubmit}
+      render={({ handleSubmit, form, submitting, pristine, values }) => (
+        <form onSubmit={handleSubmit}>
+          <Field name="title" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Title</label>
+                <input {...input} type="text" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="location" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Location</label>
+                <input {...input} type="text" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="status" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Property status</label>
+                <select {...input} name="status">
+                  <option />
+                  <option value="rent">For Rent</option>
+                  <option value="sell">For Sell</option>
+                </select>
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="price" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Price</label>
+                <input {...input} type="number" />
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          <Field name="currency" validate={required}>
+            {({ input, meta }) => (
+              <div>
+                <label>Currency</label>
+                <select {...input} name="currency">
+                  <option />
+                  <option value="usd">USD</option>
+                  <option value="mxp">Mexican Pesos</option>
+                  <option value="eur">Euros</option>
+                </select>
+                {meta.error && meta.touched && <span>{meta.error}</span>}
+              </div>
+            )}
+          </Field>
+          {values && values.status === "rent" ? periodQuestion : <div></div>}
+          <div>
+            <label>Description</label>
+            <Field name="description" component="textarea" />
+          </div>
+          <div>
+            <ImageUploader
+              withIcon={false}
+              buttonText="Choose images"
+              onChange={onDrop}
+              imgExtension={[".jpg", ".gif", ".png", ".gif"]}
+              maxFileSize={5242880}
+              withPreview={true}
+            />
+          </div>
+          <div className="buttons">
+            <button type="submit" disabled={submitting || pristine}>
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+    />
+  </Styles>
+);
+
+export default CreateProperty;
